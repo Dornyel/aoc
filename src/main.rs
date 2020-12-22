f40aaccc311075af40e80ae59ee24783f64ec3c1
dornyel <sandoy@pm.me>
2020-12-22 18:34:31 +1300

    completed aoc day 4

--- src/main.rs
+++ src/main.rs
@@ -29,47 +29,45 @@ impl Default for PassportValid {
 impl PassportValid {
     pub fn check_fields(mut self, c: &&str, l: &str) -> PassportValid {
         if *c == "byr" {
-            if l[4..8].trim().parse::<i32>().unwrap() >= 1920 && l[4..8].trim().parse::<i32>().unwrap() <= 2002 {
+            if l[4..8].trim().parse::<i32>().unwrap() >= 1920 && 
+            l[4..8].trim().parse::<i32>().unwrap() <= 2002 {
                 self.byr = true;
-            }
+            } else {self.byr = false;}
 
         } else if *c == "iyr" {
-            if l[4..8].trim().parse::<i32>().unwrap() >= 2010 && l[4..8].trim().parse::<i32>().unwrap() <= 2020 {
+            if l[4..8].trim().parse::<i32>().unwrap() >= 2010 && 
+            l[4..8].trim().parse::<i32>().unwrap() <= 2020 {
                 self.iyr = true;
-            }
+            } else {self.iyr = false;}
 
         } else if *c == "eyr" {
-            if l[4..8].trim().parse::<i32>().unwrap() >= 2020 && l[4..8].trim().parse::<i32>().unwrap() <= 2030 {
+            if l[4..8].trim().parse::<i32>().unwrap() >= 2020 && 
+            l[4..8].trim().parse::<i32>().unwrap() <= 2030 {
                 self.eyr = true;
-            }
+            } else {self.eyr = false;}
 
         } else if *c == "hgt" {
-         //println!("{:?} height: {:?}", l, l.len());
-            if l.len() == 8 {
+                    if l.len() == 8 {
                 if (String::from(&l[5..])).contains("in") {
-                    //println!("{:?}", l[4..6]);
-                    //println!("{:?}", String::from(&l[4..6]).trim().parse::<i32>().unwrap());
-                    if String::from(&l[4..6]).trim().parse::<i32>().unwrap() >= 59 && String::from(&l[4..6]).trim().parse::<i32>().unwrap() <= 76 {
-                        //println!("aadsad");
+                    if String::from(&l[4..6]).trim().parse::<i32>().unwrap() >= 59 && 
+                    String::from(&l[4..6]).trim().parse::<i32>().unwrap() <= 76 {
                         self.hgt = true;
-                    }
+                    } else {self.hgt = false;}
                 }
             } else if l.len() == 9 {
-                if (String::from(&l[6..])).contains("cm") {
-                    //println!("{:?}", String::from(&l[4..7]).trim().parse::<i32>().unwrap());
-                    
-                    if String::from(&l[4..7]).trim().parse::<i32>().unwrap() >= 150i32 && String::from(&l[4..7]).trim().parse::<i32>().unwrap() <= 193i32 {
-                        //println!("aaaa");
+                if (String::from(&l[6..])).contains("cm") {                    
+                    if String::from(&l[4..7]).trim().parse::<i32>().unwrap() >= 150 && 
+                    String::from(&l[4..7]).trim().parse::<i32>().unwrap() <= 193 {
                         self.hgt = true;
-                    }
-                } else {return self}
+                    } else {self.hgt = false;}
+                } else {self.hgt = false;}
             }
              else {
                 return self
             }
 
         } else if *c == "hcl" {
-            let hcl = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+            let hcl = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
             let mut correct = 0;
             if l.len() == 11 {
                 if l.chars().nth(4) == Some('#') {
@@ -80,17 +78,19 @@ impl PassportValid {
                             }
                         }
                     }
-
                 }
             }
             if correct == 6 {
+
                 self.hcl = true;
+            } else {
+                self.hcl = false;
             }
-        }
-        else if *c == "ecl" {
+
+        } else if *c == "ecl" {
             if l.len() == 7 {
                 for i in &ECL {
-                    if *i == &l[4..] {
+                    if *i == String::from(&l[4..]) {
                         self.ecl = true;
                     }
                 }
@@ -99,41 +99,42 @@ impl PassportValid {
             }
             
         } else if *c == "pid" {
-            let base = 10;
-            let mut digits = 0;
-            //println!("{:?}", l);
-            let mut num: i32 = match l[4..].trim().parse::<i32>() {
-                Ok(num) => num,
-                Err(_) => return self
-            };
-            while num != 0 {
-                num /= base;
-                digits += 1;
+            let chars: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            let mut counter = 0;
+            let num2 = String::from(&l[4..]);
+            if num2.len() == 9{
+                for i in num2.chars() {
+                    for j in &chars {
+                        if i == *j {
+                            counter += 1;
+                        }
+                    }
             }
-            if digits == 8 {
+        }
+            if counter == 9 {
                 self.pid = true;
+            } else {
+                self.pid = false;
             }
         }
         return self;
     }
 }
+
     pub fn is_valid(p: &PassportValid) -> bool {
-        //println!("{:?}", p);
-        if {p.byr == true &&
+
+        if p.byr == true &&
             p.iyr == true &&
             p.eyr == true &&
             p.hgt == true &&
             p.hcl == true &&
             p.ecl == true &&
-            p.pid == true } {
+            p.pid == true {
                 return true;
             } else {
                 return false;
             }
     }
-
-
-
 
 fn main() {
 
@@ -170,10 +171,7 @@ fn main() {
             for k in 0..lines2[i].len() {
                 if lines2[i][k] == "" {
                     if counter == FIELDS.len() {
-                        //println!("aaa");
-                        //println!("{:?}", passport);
                         if is_valid(&passport) == true {
-                            println!("{:?}", passport);
                             correct += 1;
                             passport = PassportValid {
                                 byr: false,
@@ -184,16 +182,22 @@ fn main() {
                                 ecl: false,
                                 pid: false
                             };
+                        } else { 
+                            passport = PassportValid {
+                                byr: false,
+                                iyr: false,
+                                eyr: false,
+                                hgt: false,
+                                hcl: false,
+                                ecl: false,
+                                pid: false
+                            };
                         }
-                        
                     }
                     counter = 0;
                 } else if *j == &lines2[i][k][0..3] {
                     counter += 1;
                     passport = passport.check_fields(j, lines2[i][k]);
-                    //println!("{:?}", passport);
-                    //std::thread::sleep_ms(200);
-
                 }
             }
         }
